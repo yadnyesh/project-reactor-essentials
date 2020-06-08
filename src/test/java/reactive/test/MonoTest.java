5fc2fae6a795aa3700848c820f388bd5f6f2ddb1
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-08 14:26:46 +0530

    Created First Test ..WoW

---
+++ src/test/java/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test() {
+        log.info("Everthing working as intended....:)");
+    }
+}
