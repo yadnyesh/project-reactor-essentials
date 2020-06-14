032edb48d512a6df082619dbd30d65f0becdc9b6
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-14 08:27:54 +0530

    Subscribing to Mono with Consumer, also demonstrates Error

--- src/test/java/reactive/test/MonoTest.java
+++ src/test/java/reactive/test/MonoTest.java
@@ -24,6 +24,33 @@ public class MonoTest {
                 .verifyComplete();
 
         log.info("Mono working as intended....:)");
+    }
 
+    @Test
+    public void monoSubscribeConsumer() {
+        String name = "Yadnyesh Juvekar";
+        Mono<String> mono = Mono.just(name).log();
+        mono.subscribe(s -> log.info("Value is {}", s));
+
+        StepVerifier.create(mono)
+                .expectNext("Yadnyesh Juvekar")
+                .verifyComplete();
+
+        log.info("Mono working as intended....:)");
+    }
+
+    @Test
+    public void monoSubscribeConsumerError() {
+        String name = "Yadnyesh Juvekar";
+        Mono<String> mono = Mono.just(name)
+                .map(s -> {throw new RuntimeException("Testing mono with Error");});
+        mono.subscribe(s -> log.info("Value is {}", s), s -> log.error("something went wrong..."));
+        mono.subscribe(s -> log.info("Value is {}", s), Throwable::printStackTrace);
+
+        StepVerifier.create(mono)
+                .expectError(RuntimeException.class)
+                .verify();
+
+        log.info("Mono working as intended....:)");
     }
 }
