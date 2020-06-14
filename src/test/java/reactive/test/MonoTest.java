efbab58ddb0d47f75fde840fbe0d8c9d08c8e989
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-14 08:15:51 +0530

    Subscribing to Mono

--- src/test/java/reactive/test/MonoTest.java
+++ src/test/java/reactive/test/MonoTest.java
@@ -2,6 +2,8 @@ package reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
@@ -9,5 +11,19 @@ public class MonoTest {
     @Test
     public void test() {
         log.info("Everything working as intended....:)");
+    }
+
+    @Test
+    public void monoSubscriber() {
+        String name = "Yadnyesh Juvekar";
+        Mono<String> mono = Mono.just(name);
+        mono.subscribe();
+
+        StepVerifier.create(mono)
+                .expectNext("Yadnyesh Juvekar")
+                .verifyComplete();
+
+        log.info("Mono working as intended....:)");
+
     }
 }
